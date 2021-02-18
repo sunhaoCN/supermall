f1acf3f7bf02c4cb02e9755ab782dbd750300943
sunhaoCN <sunhaoonline@163.com>
2021-02-18 20:14:24 +0800

    '更新0218'

---
+++ src/components/content/MainTabBar/MainTabBar.vue
@@ -0,0 +1,32 @@
+<template>
+  <tab-bar>
+    <tab-bar-item link="/quote" active-color="deepPink">
+      <img slot="item-icon" src="~assets/img/tabbar/home.svg" alt="">
+      <img slot="item-icon-active" src="~assets/img/tabbar/home_active.svg" alt="">
+      <div slot="item-text">报价</div>
+    </tab-bar-item>
+    <tab-bar-item link="/profile" active-color="deepPink">
+      <img slot="item-icon" src="~assets/img/tabbar/profile.svg" alt="">
+      <img slot="item-icon-active" src="~assets/img/tabbar/profile_active.svg" alt="">
+      <div slot="item-text">我的</div>
+    </tab-bar-item>
+  </tab-bar>
+</template>
+
+<script>
+
+import TabBar from "components/tabbar/TabBar";
+import TabBarItem from "components/tabbar/TabBarItem";
+
+export default {
+  name: "MainTabBar",
+  components: {
+    TabBar,
+    TabBarItem
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
