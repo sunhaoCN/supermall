ef82cae72401919f287b2de3c0454370d39b98d1
sunhaoCN <sunhaoonline@163.com>
2021-02-18 22:35:51 +0800

    0218 2235更新

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,35 +1,110 @@
 <template>
   <div id="home">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
+    <recommend-view :recommends="recommends"/>
+    <feature-view/>
+    <tab-control :titles="['流行', '新款', '精选']" class="tab-control"/>
+
+    <ul>
+      <li>....</li>
+      <li>....</li>
+      <li>....</li>
+      <li>....</li>
+      <li>....</li>
+      <li>....</li>
+      <li>....</li>
+      <li>....</li>
+      <li>....</li>
+      <li>....</li>
+      <li>....</li>
+      <li>....</li>
+      <li>....</li>
+      <li>....</li>
+      <li>....</li>
+      <li>....</li>
+      <li>....</li>
+      <li>....</li>
+      <li>....</li>
+      <li>....</li>
+      <li>....</li>
+      <li>....</li>
+      <li>....</li>
+      <li>....</li>
+      <li>....</li>
+      <li>....</li>
+      <li>....</li>
+      <li>....</li>
+      <li>....</li>
+      <li>....</li>
+      <li>....</li>
+      <li>....</li>
+      <li>....</li>
+      <li>....</li>
+      <li>....</li>
+      <li>....</li>
+      <li>....</li>
+      <li>....</li>
+    </ul>
   </div>
 </template>
 
 <script>
+  //子组件
+  import HomeSwiper from "./childrenComps/HomeSwiper";
+  import RecommendView from "./childrenComps/RecommendView";
+  import FeatureView from "./childrenComps/FeatureView";
+
+  //公共组件
   import NavBar from "components/common/navbar/NavBar";
+  import TabControl from "components/content/tabControl/TabControl";
+
   import {getHomeMultiDate} from "network/home";
 
   export default {
     name: "Home",
     components: {
-      NavBar
+      HomeSwiper,
+      RecommendView,
+      FeatureView,
+
+      NavBar,
+      TabControl
     },
     data() {
       return {
-        result: null
+        banners: [],
+        recommends: []
       }
     },
     created() {
       //1.请求多个数据
       getHomeMultiDate().then( res => {
-          this.result = res;
+          this.banners = res.data.banner.list;
+          this.recommends = res.data.recommend.list;
       })
     }
   }
 </script>
 
 <style scoped>
+  #home {
+    padding-top: 44px;
+  }
+
   .home-nav {
     background-color: var(--color-tint);
     color: #fff;
+
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 9;
+  }
+
+  .tab-control {
+    position: sticky;
+    top: 44px;
   }
 </style>
